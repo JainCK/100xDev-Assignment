73d759ab5439df2e7ac33ace22650a69ecd4ae14
Jain K <kuriakosejain456@gmail.com>
2023-12-22 21:56:36 +0530

    admin middleware completed

--- week-3/03-mongo/middleware/admin.js
+++ week-3/03-mongo/middleware/admin.js
@@ -1,7 +1,38 @@
+const { Admin } = require("../db");
+
 // Middleware for handling auth
-function adminMiddleware(req, res, next) {
+async function adminMiddleware(req, res, next) {
     // Implement admin auth logic
     // You need to check the headers and validate the admin from the admin DB. Check readme for the exact headers to be expected
+    try{
+        const username = req.headers['username'];
+        const password = req.headers['password'];
+
+        if(!username || !password){
+            res.status(404).json({
+                message: "Username or Password not found",
+            })
+        }
+
+        const user = await Admin.findOne({username: username});
+        if(!user){
+            res.status(404).json({
+                message: "This route is only for admins !!",
+            })
+        }
+
+        if(user.password !== password){
+            res.status(403).json({
+                message: "Incorrect Password",
+            })
+        }
+
+        next();
+    }catch(err){
+        res.status(500).json({
+            "error": "Could not verify the user",
+        })
+    }
 }
 
 module.exports = adminMiddleware;
