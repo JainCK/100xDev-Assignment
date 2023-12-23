8a22664fd393524db59aabb41ebe396b126a1b83
Jain K <kuriakosejain456@gmail.com>
2023-12-23 18:38:17 +0530

    db schema updated

--- week-3/04-mongo-with-jwt-auth/db/index.js
+++ week-3/04-mongo-with-jwt-auth/db/index.js
@@ -1,19 +1,46 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 // Connect to MongoDB
-mongoose.connect('your-mongodb-url');
+mongoose.connect(process.env.MONGO_URI);
 
 // Define schemas
 const AdminSchema = new mongoose.Schema({
     // Schema definition here
+    username: String,
+    password: String,
+    courseCreated: [{
+        type:mongoose.Schema.Types.ObjectId,
+        ref: "Course",
+    }],
+    token: string
 });
 
 const UserSchema = new mongoose.Schema({
-    // Schema definition here
+    username: String,
+    password: String,
+    courseEnrolled: [{
+        type:mongoose.Schema.Types.ObjectId,
+        ref: "Course",
+    }],
+    token: string
 });
 
 const CourseSchema = new mongoose.Schema({
     // Schema definition here
+    title: String,
+    price: Number,
+    description: String,
+    imageLink: String,
+    createdBy: String,
+    published: {
+        type: Boolean,
+        default: false,
+    },
+    usersEnrolled: [{
+        type: mongoose.Schema.type.ObjectId,
+        ref: "User",
+    }]
 });
 
 const Admin = mongoose.model('Admin', AdminSchema);
