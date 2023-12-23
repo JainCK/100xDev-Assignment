27cd008ca3916874e9088c8bebea4b35d709e767
Jain K <kuriakosejain456@gmail.com>
2023-12-23 18:45:32 +0530

    jwt added, admin middleware completed

--- week-3/04-mongo-with-jwt-auth/middleware/admin.js
+++ week-3/04-mongo-with-jwt-auth/middleware/admin.js
@@ -1,7 +1,25 @@
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
+
 // Middleware for handling auth
 function adminMiddleware(req, res, next) {
     // Implement admin auth logic
     // You need to check the headers and validate the admin from the admin DB. Check readme for the exact headers to be expected
+try {
+    const tokenHd = req.headers["authorization"];
+    const token = tokenHd.split("")[1];
+
+    const jwtPassword= process.env.ADMIN_PASS;
+    const decode = jwt.verify(token, jwtPassword);
+
+    next();
+}catch {
+    res.status(500).json({
+        error: "token invalid",
+        message: err.message,
+    })
+}
+
 }
 
 module.exports = adminMiddleware;
