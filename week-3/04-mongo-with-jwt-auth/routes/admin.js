5cb9f1b76ed46f7dad5dddd849b401cfcd035324
Jain K <kuriakosejain456@gmail.com>
2023-12-23 19:29:11 +0530

    added aign in route for Admin

--- week-3/04-mongo-with-jwt-auth/routes/admin.js
+++ week-3/04-mongo-with-jwt-auth/routes/admin.js
@@ -1,10 +1,43 @@
 const { Router } = require("express");
 const adminMiddleware = require("../middleware/admin");
+const {Admin, Course} = require("../db")
+const jwt = require('jsonwebtoken');
 const router = Router();
+require('dotenv').config();
 
 // Admin Routes
-app.post('/signup', (req, res) => {
+app.post('/signup', async (req, res) => {
     // Implement admin signup logic
+    try {
+        const username = req.body.username;
+        const password = req.body.password;
+
+        if(!username || !password) {
+            res.status(404).json ({
+                message: "Username or Password not Found"
+            });
+        }
+
+        const jwtPassword = process.env.jwtPassword;
+        const token = jwt.sign({username: username, password: password}, jwtPassword)
+
+        const updateUser = await Admin.findOneAndUpdate(
+            {username: username},
+            {
+                token: token,
+            },
+            {new: true}
+        )
+
+        res.status(200).json ({
+            token: token,
+        })
+
+    } catch(err) {
+        res.status(500).json({
+            message: "Admin could not be signed"
+        })
+    }
 });
 
 app.post('/signin', (req, res) => {
