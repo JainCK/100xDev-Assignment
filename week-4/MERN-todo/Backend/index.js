25ed853c465c4a55f6a11ae8e920060d6dc4e3b1
Jain K <kuriakosejain456@gmail.com>
2024-01-02 16:02:46 +0530

    put req added

--- week-4/MERN-todo/Backend/index.js
+++ week-4/MERN-todo/Backend/index.js
@@ -1,22 +1,33 @@
 const express = require("express");
-const { createTodo } = require("./types");
+const { createTodo, updateTodo } = require("./types");
 const app = express();
 
 app.use(express.json());
 
 app.get('/todos', (req, res) => {
     const createPayload = req.body;
-    const parsedTodo = createTodo.safeParse(createPayload);
-    if(!parsedTodo.success){
+    const parsedPayload = createTodo.safeParse(createPayload);
+    if(!parsedPayload.success){
         res.status(411).json({
             msg: "wrong inputs"
         })
     }
 
+
 });
 
 app.put('/completed', (req, res) => {
+    const updatePayload = req.body;
+    const parsedPayload = updateTodo.safeParse(updatePayload);
+    if(!parsedPayload.success){
+        res.status(411).json({
+            msg: "You sent the wrong inputs",
+        })
+        return;
+    }
 
-});
+
+    
+    });
 
 app.listen(3000)
