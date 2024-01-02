36e32c04d4c3d92482a24064419be899f497cd81
Jain K <kuriakosejain456@gmail.com>
2024-01-02 15:58:22 +0530

    imported types

--- week-4/MERN-todo/Backend/index.js
+++ week-4/MERN-todo/Backend/index.js
@@ -1,10 +1,14 @@
 const express = require("express");
+const { createTodo } = require("./types");
 const app = express();
 
 app.use(express.json());
 
 app.get('/todos', (req, res) => {
+    const createPayload = req.body;
+    const parsedTodo = createTodo;
 
+    
 });
 
 app.put('/completed', (req, res) => {
