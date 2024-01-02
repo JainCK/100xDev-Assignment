6f7201f81f8d2f41724e579430dbe5f1f465e3dc
Jain K <kuriakosejain456@gmail.com>
2024-01-02 16:07:19 +0530

    Added todo schema

--- week-4/MERN-todo/Backend/db.js
+++ week-4/MERN-todo/Backend/db.js
@@ -1,3 +1,13 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI);
+
+const todoSchema = mongoose.Schema({
+    title: String,
+    description: String,
+    completed: Boolean,
+})
+
+const todo = mongoose.model('todos', todoSchema);
+
+module.exports = todo;
