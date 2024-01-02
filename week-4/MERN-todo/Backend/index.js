28fb69cd372c97fbb111e5e1b5b77cb2e0bcaa0a
Jain K <kuriakosejain456@gmail.com>
2024-01-02 16:14:53 +0530

    added mongo connectivity

--- week-4/MERN-todo/Backend/index.js
+++ week-4/MERN-todo/Backend/index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const { createTodo, updateTodo } = require("./types");
+const { todo } = require ('./db');
 const app = express();
+const cors = require('cors');
 
 app.use(express.json());
+app.use(cors());
 
 app.get('/todos', async (req, res) => {
     const createPayload = req.body;
