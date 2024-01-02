613db631df1541a5aaeccb3c7868af03dccffe5b
Jain K <kuriakosejain456@gmail.com>
2024-01-02 15:50:29 +0530

    added methods

--- week-4/MERN-todo/Backend/index.js
+++ week-4/MERN-todo/Backend/index.js
@@ -1,6 +1,14 @@
 const express = require("express");
+const app = express();
 
-const app = express;
+app.use(express.json());
 
+app.get('/todos', (req, res) => {
+
+});
+
+app.put('/completed', (req, res) => {
+
+});
 
 app.listen(3000)
