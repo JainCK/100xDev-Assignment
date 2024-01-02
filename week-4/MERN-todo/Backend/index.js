09d4c3202b452930955c9139d52aadafb6e2c633
Jain K <kuriakosejain456@gmail.com>
2024-01-02 16:11:57 +0530

    Added mongodb insertion code

--- week-4/MERN-todo/Backend/index.js
+++ week-4/MERN-todo/Backend/index.js
@@ -4,19 +4,29 @@ const app = express();
 
 app.use(express.json());
 
-app.get('/todos', (req, res) => {
+app.get('/todos', async (req, res) => {
     const createPayload = req.body;
     const parsedPayload = createTodo.safeParse(createPayload);
     if(!parsedPayload.success){
         res.status(411).json({
             msg: "wrong inputs"
         })
+        return;
     }
 
+    await todo.create({
+        title: createPayload.title,
+        description: createPayload.description,
+        completed: false,
+    });
+
+    res.json({
+        msg: "Todo created"
+    })
 
 });
 
-app.put('/completed', (req, res) => {
+app.put('/completed', async (req, res) => {
     const updatePayload = req.body;
     const parsedPayload = updateTodo.safeParse(updatePayload);
     if(!parsedPayload.success){
@@ -26,8 +36,16 @@ app.put('/completed', (req, res) => {
         return;
     }
 
+    await todo.update({
+        _id: req.body.id
+    }, {
+        completed: true
+    })
 
-    
+    res.json({
+        msg: 'Todo marked as completed'
+    })
+
     });
 
-app.listen(3000)
+app.listen(3000);
