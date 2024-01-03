a557300054ca4197067dcc6a6a09a50451b1d9a9
Jain K <kuriakosejain456@gmail.com>
2024-01-03 15:14:42 +0530

    added input and button componment

---
+++ week-4/MERN-todo/Frontend/src/componenets/CreateTodo.jsx
@@ -0,0 +1,8 @@
+export function CreateTodo () {
+    return <div>
+        <input type = "text" placeholder="title"></input> <br />
+        <input type = "text" placeholder="Desc"></input> <br />
+
+        <button>Add A Todo</button>
+    </div>
+}
