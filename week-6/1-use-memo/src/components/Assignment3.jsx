ccc64a2492feb53ead840fbb4c4eddf7abe112fd
Jain K <kuriakosejain456@gmail.com>
2024-01-11 18:00:25 +0530

    Assignment-3 memo completed

--- week-6/1-use-memo/src/components/Assignment3.jsx
+++ week-6/1-use-memo/src/components/Assignment3.jsx
@@ -8,11 +8,15 @@ const Assignment3 = () => {
         { name: 'Chips', value: 20 },
         { name: 'Onion', value: 30 },
         { name: 'Tomato', value: 30 },
+        { name: 'Potato', value: 40 },
+        { name: 'Biscuits', value: 60},
         // Add more items as needed
     ]);
 
     // Your code starts here
-    const totalValue = 0;
+    const totalValue = useMemo(() => {
+        return items.reduce((acc, item) => acc + item.value, 0)
+    },[items]);
     // Your code ends here
     return (
         <div>
