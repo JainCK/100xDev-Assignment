c09d017450cdccd32db8ad899a683d01e57e903f
Jain K <kuriakosejain456@gmail.com>
2024-01-14 19:07:53 +0530

    intinitail code

--- week-6/3-use-ref/src/components/Assignment1.jsx
+++ week-6/3-use-ref/src/components/Assignment1.jsx
@@ -15,9 +15,32 @@ export function Assignment1() {
     };
 
     return (
-        <div>
-            <input type="text" placeholder="Enter text here" ref={inputRef}/>
-            <button onClick={handleButtonClick}>Focus Input</button>
+        <div style={styles.conatiner}>
+            <input type="text" placeholder="Enter text here" ref={inputRef} style={styles.input}/>
+            <button onClick={handleButtonClick} style={styles.Btn}>Focus Input</button>
         </div>
     );
 };
+
+const styles = {
+    conatiner: {
+        border: '1px solid #ddd',
+      borderRadius: '8px',
+      padding: '20px',
+      margin: '20px',
+      maxWidth: '400px',
+      boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
+      backgroundColor: '#f8f9fa'
+    },
+    Btn : {
+        marginLeft: '10px',
+        height: '30px',
+        fontSize: '15px',
+        borderRadius: '5px'
+    },
+    input: {
+        height: '30px',
+        borderRadius: '5px',
+        fontSize:'18px'
+    }
+ }
--- week-6/3-use-ref/src/components/Assignment2.jsx
+++ week-6/3-use-ref/src/components/Assignment2.jsx
@@ -1,19 +1,45 @@
 import React, { useState, useCallback } from 'react';
+import { useRef } from 'react';
 
 // Create a component that tracks and displays the number of times it has been rendered. Use useRef to create a variable that persists across renders without causing additional renders when it changes.
 
 export function Assignment2() {
-    const [, forceRender] = useState(0);
+    const [render, forceRender] = useState(0);
+    const countRef = useRef(0)
 
     const handleReRender = () => {
         // Update state to force re-render
+
+        countRef.current +=1;
+
         forceRender(Math.random());
     };
 
     return (
-        <div>
-            <p>This component has rendered {0} times.</p>
-            <button onClick={handleReRender}>Force Re-render</button>
+        <div style={styles.conatiner}>
+            <p style={styles.para}>This component has rendered {countRef.current} times.</p>
+            <button onClick={handleReRender} style={styles.Btn}>Force Re-render</button>
         </div>
     );
 };
+
+const styles = {
+    conatiner: {
+        border: '1px solid #ddd',
+      borderRadius: '8px',
+      padding: '20px',
+      margin: '20px',
+      maxWidth: '400px',
+      boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
+      backgroundColor: '#f8f9fa'
+    }, 
+    para : {
+        fontSize:'20px'
+    },
+    Btn : {
+        marginLeft: '10px',
+        height: '30px',
+        fontSize: '15px',
+        borderRadius: '5px'
+    }
+ }
