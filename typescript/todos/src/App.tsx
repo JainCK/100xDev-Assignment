7eedebb016d21ac3f89bf1fb3510cc8c82c8cfa0
Jain K <kuriakosejain456@gmail.com>
2024-04-10 01:05:34 +0530

    react ts code

---
+++ typescript/todos/src/App.tsx
@@ -0,0 +1,27 @@
+import "./App.css";
+
+function App() {
+  return <></>;
+}
+
+// Todo.tsx
+interface TodoType {
+  title: string;
+  description: string;
+  done: boolean;
+}
+
+interface TodoInput {
+  todo: TodoType;
+}
+
+function Todo({ todo }: TodoInput) {
+  return (
+    <div>
+      <h1>{todo.title}</h1>
+      <h2>{todo.description}</h2>
+    </div>
+  );
+}
+
+export default App;
