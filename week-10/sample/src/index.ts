dec6f6b4e78f0aba1214d8eeefad248a3b4ff718
Jain K <kuriakosejain456@gmail.com>
2024-04-15 09:29:21 +0530

    connect succesfull

--- week-10/sample/src/index.ts
+++ week-10/sample/src/index.ts
@@ -1,13 +1,22 @@
 import { Client } from "pg";
 
-async function insertData() {
-  const client = new Client({
-    connectionString:
-  });
+const client = new Client({
+  connectionString:
+    "postgresql://kuriakosejain456:tSbBXqe2TI5M@ep-orange-art-46456096.ap-southeast-1.aws.neon.tech/Test?sslmode=require",
+});
 
-  try {
-    await client.connect();
-    const insertQuery =
-      "INSERT INTO users (username, email, password) VALUES ('username2', 'user3@example.com', 'user_password');";
-  } catch (error) {}
+async function createUserTable() {
+  await client.connect();
+  const result = await client.query(`
+      CREATE TABLE users (
+        id SERIAL PRIMARY KEY,
+        username VARCHAR(50) UNIQUE NOT NULL,
+        email VARCHAR(255) UNIQUE NOT NULL,
+        password VARCHAR(255) NOT NULL,
+        created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+      );
+      `);
+  console.log(result);
 }
+
+createUserTable();
