e1d0ae25ded98b328af22fb2e67f704a6ee79736
Jain K <kuriakosejain456@gmail.com>
2024-05-20 21:21:09 +0530

    layout fix

--- week-14/w-14/app/layout.tsx
+++ week-14/w-14/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Navbar />
+        <div className="flex flex-col items-center justify-between mx-auto w-[30%] py-12">
+        {children}
+        </div>
+      </body>
     </html>
   );
 }
